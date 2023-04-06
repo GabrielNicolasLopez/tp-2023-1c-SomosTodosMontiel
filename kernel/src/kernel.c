692d870d767cf573a21a3bd199e66ed67bf02ecb
Mariano Gutierrez <mgutierrezfilgueira@frba.utn.edu.ar>
2023-04-06 15:48:27 +0000

    Prueba debug
    Agregue codigo de prueba para debugear.
    Los archivos .log ahora se crean en la carpeta cfg.

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -6,8 +6,13 @@ int main(int argc, char ** argv){
     else{  
         char* modulo = "kernel"; 
         char* path = "./cfg/";
+        char* extension= ".log";
         char buffer1[40] = "./cfg/";
+        char buffer2[40];
         char* pathcompleto = strcat(buffer1, modulo);
+        pathcompleto = strcat(buffer1, extension);
+        char* moduloConExtension = strcat(buffer2, modulo);
+        moduloConExtension = strcat(buffer2, extension);
         printf("%s", buffer1);
         t_log* logger = log_create(buffer1, modulo, true, LOG_LEVEL_INFO);
         log_info(logger, "Soy el modulo %s! guarde el logger en la siguiente ruta:%s. %s", modulo, buffer1, mi_funcion_compartida());
