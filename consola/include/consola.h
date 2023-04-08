9ae6cc4e20213e78b2e2e32a7b48af6bd6595aa0
Mariano Gutierrez <mgutierrezfilgueira@frba.utn.edu.ar>
2023-04-08 17:59:21 +0000

    Fix template

--- consola/include/consola.h
+++ consola/include/consola.h
@@ -2,6 +2,7 @@
 #define PROCESO1_H
 #include <stdio.h>
 #include <commons/log.h>
+#include <commons/config.h>
 #include <stdbool.h>
 #include "shared_utils.h"
 #include "tests.h"
--- consola/src/consola.c
+++ consola/src/consola.c
@@ -1,21 +1,33 @@
 #include "consola.h"
 
-int main(int argc, char ** argv){
-    if(argc > 1 && strcmp(argv[1],"-test")==0)
-        return run_tests();
-    else{  
-        char* modulo = "consola"; 
-        char* path = "./cfg/";
-        char* extension= ".log";
-        char buffer1[40] = "./cfg/";
-        char buffer2[40];
-        char* pathcompleto = strcat(buffer1, modulo);
-        pathcompleto = strcat(buffer1, extension);
-        char* moduloConExtension = strcat(buffer2, modulo);
-        moduloConExtension = strcat(buffer2, extension);
-        printf("%s", buffer1);
-        t_log* logger = log_create(buffer1, modulo, true, LOG_LEVEL_INFO);
-        log_info(logger, "Soy el modulo %s! guarde el logger en la siguiente ruta:%s. %s", modulo, buffer1, mi_funcion_compartida());
-        log_destroy(logger);
-    } 
+#define CONSOLA_CONFIG_PATH "./cfg/consola.cfg"
+#define CONSOLA_LOG_PATH "./cfg/consola.log"
+#define CONSOLA_MODULE_NAME "consola"
+
+int main(int argc, char* argv[]){
+
+
+    t_log *logger = log_create(CONSOLA_LOG_PATH, CONSOLA_MODULE_NAME, true, LOG_LEVEL_INFO);
+
+    t_config* config = config_create(CONSOLA_CONFIG_PATH);
+    char* ip = config_get_string_value(config, "IP");
+    char* puerto = config_get_string_value(config, "PUERTO");
+
+
+    log_info(logger, "La IP es %s", ip);
+    log_info(logger, "El puerto es %s", puerto);
+    log_info(logger, "EL argc es %d", argc);
+    for(int i =0;i<10; i++){
+    log_info(logger, "EL argv[%d] es %s",i, argv[i]);
+    }
+    log_destroy(logger);
+
+    int i;
+    printf("argc = %d\n", argc);
+
+    printf("Vamos a ver que hay en argv[]\n");
+
+    for(i=0; i<argc+1; i++){
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
 }
