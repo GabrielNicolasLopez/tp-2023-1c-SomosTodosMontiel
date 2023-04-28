b6089f6cca4b8d0e3960065985c412f9523a4523
Mariano Gutierrez <mgutierrezfilgueira@frba.utn.edu.ar>
2023-04-28 16:16:00 +0000

    Add log path a filesystem

--- filesystem/include/filesystem.h
+++ filesystem/include/filesystem.h
@@ -10,7 +10,7 @@
 
 #define CONFIG_PATH "./cfg/filesystem.cfg"
 //#define LOG_PATH "./cfg/filesystem.log" LOG QUE PERSISTE EN EL REPO REMOTO
-#define LOG_PATH "./cfg/filesystem.log" //LOG QUE NO SE SUBE AL REPO REMOTO
+#define LOG_PATH "./cfg/filesystemPrueba.log" //LOG QUE NO SE SUBE AL REPO REMOTO
 #define MODULE_NAME "Filesystem"
 
 typedef struct
