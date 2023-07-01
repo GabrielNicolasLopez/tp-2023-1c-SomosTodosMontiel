63b0e094bc7cd8e7d44495d779b67eeab4a04844
IvanErazun <ierazun@frba.utn.edu.ar>
2023-07-01 22:53:26 +0000

    Nuevo esquema de MAKES

---
+++ consola/settings.mk
@@ -0,0 +1,20 @@
+# Libraries
+LIBS= shared commons pthread readline cunit rt
+
+# Custom libraries' paths
+SHARED_LIBPATHS=
+STATIC_LIBPATHS=../shared
+
+# Compiler flags
+CDEBUG=-g -Wall -DDEBUG
+CRELEASE=-O3 -Wall -DNDEBUG
+
+# Arguments when executing with start, memcheck or helgrind
+ARGS=
+
+# Valgrind flags
+MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck.log"
+HELGRIND_FLAGS=
+
+# Source files (*.c) to be excluded from tests compilation
+TEST_EXCLUDE=src/main.c
